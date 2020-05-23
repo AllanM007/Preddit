484970964855d4b3c882379ccca7c026258cf987
AllanM007 <mwaranguallan345@gmail.com>
2020-05-24 00:07:17 +0300

    add tests

--- red1/tests.py
+++ red1/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from red1.models import Subweddit
 
-# Create your tests here.
+
+class SubwedditTestCase(TestCase):
+    def testSubweddit(self):
+        subweddit = Subweddit(name="w/cars", created_on="2020-4-20", bio="I am Odin", guidelines="OdinForce", slug="cars")
+        self.assertEqual(subweddit.name, "w/cars")
+        self.assertEqual(subweddit.created_on, "2020-4-20")
+        self.assertEqual(subweddit.bio, "I am Odin")
+        self.assertEqual(subweddit.guidelines, "OdinForce")
+        self.assertEqual(subweddit.slug, "cars")
